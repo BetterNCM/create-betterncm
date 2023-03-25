40f188096b782d5b92007638b6a04bc4bbf210ce
MicroBlock <microblock@qq.com>
2023-03-25 17:42:25 +0800

    impl

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-const childProcess = require('child_process');
+#!/usr/bin/env node
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+var prompt = require('prompt');
+const { readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
 
-async function init(){
+async function init() {
+    const cwd = process.cwd();
+
+    const { PluginName, Author } = await prompt.get(['PluginName', 'Author',]);
+
     console.log("Cloning BetterNCM boilerplate...");
-    await exec("git clone https://github.com/BetterNCM/boilerplate");
+    await exec("git clone https://github.com/BetterNCM/boilerplate", { cwd });
     console.log("Installing dependencies...");
-    await exec("cmd /c cd boilerplate & npm install yarn -g & yarn");
-    try{
-        await exec("cmd /c cd boilerplate & code .");
-    }catch(e){
+    await exec("cmd /c cd boilerplate & npm install yarn -g & yarn", { cwd });
+
+    // replace boilerplate with plugin name
+    const replaceFile = (filename, from, to) => {
+        let str = readFileSync(join(cwd, filename)).toString("utf8");
+        while (str.includes(from)) str = str.replace(from, to);
+        writeFileSync(join(cwd, filename), str);
+    }
+
+    replaceFile("manifest.json","@betterncm/boilerplate", PluginName);
+    replaceFile("manifest.json","MicroBlock", Author);
+
+    // rename boilerplate
+    await exec(`ren boilerplate ${PluginName}`, { cwd });
+
+    try {
+        await exec("cmd /c cd boilerplate & code .", { cwd });
+    } catch (e) {
         console.log("VSCode not found, skipping...");
     }
 }
